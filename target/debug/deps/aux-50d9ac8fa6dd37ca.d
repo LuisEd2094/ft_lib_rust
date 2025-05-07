18caaf97eef6befe4e7de241d77ae9a5cc1ae0d8
Luis <luissoto2094@gmail.com>
2025-05-07 11:44:13 +0200

    feat: first commit

---
+++ target/debug/deps/aux-50d9ac8fa6dd37ca.d
@@ -0,0 +1,14 @@
+/home/luis/proyects/ft_lib_rust/target/debug/deps/libaux-50d9ac8fa6dd37ca.rmeta: src/lib.rs src/aux/mod.rs src/aux/ft_sqrt.rs src/aux/ft_powi.rs src/aux/ft_cbrt.rs src/aux/ft_atan.rs src/aux/ft_cos.rs src/aux/ft_abs.rs
+
+/home/luis/proyects/ft_lib_rust/target/debug/deps/libaux-50d9ac8fa6dd37ca.rlib: src/lib.rs src/aux/mod.rs src/aux/ft_sqrt.rs src/aux/ft_powi.rs src/aux/ft_cbrt.rs src/aux/ft_atan.rs src/aux/ft_cos.rs src/aux/ft_abs.rs
+
+/home/luis/proyects/ft_lib_rust/target/debug/deps/aux-50d9ac8fa6dd37ca.d: src/lib.rs src/aux/mod.rs src/aux/ft_sqrt.rs src/aux/ft_powi.rs src/aux/ft_cbrt.rs src/aux/ft_atan.rs src/aux/ft_cos.rs src/aux/ft_abs.rs
+
+src/lib.rs:
+src/aux/mod.rs:
+src/aux/ft_sqrt.rs:
+src/aux/ft_powi.rs:
+src/aux/ft_cbrt.rs:
+src/aux/ft_atan.rs:
+src/aux/ft_cos.rs:
+src/aux/ft_abs.rs:
---
+++ target/debug/deps/aux-648ade97552863fe.d
@@ -0,0 +1,12 @@
+/home/luis/proyects/ft_lib_rust/target/debug/deps/aux-648ade97552863fe: src/lib.rs src/aux/mod.rs src/aux/ft_sqrt.rs src/aux/ft_powi.rs src/aux/ft_cbrt.rs src/aux/ft_atan.rs src/aux/ft_cos.rs src/aux/ft_abs.rs
+
+/home/luis/proyects/ft_lib_rust/target/debug/deps/aux-648ade97552863fe.d: src/lib.rs src/aux/mod.rs src/aux/ft_sqrt.rs src/aux/ft_powi.rs src/aux/ft_cbrt.rs src/aux/ft_atan.rs src/aux/ft_cos.rs src/aux/ft_abs.rs
+
+src/lib.rs:
+src/aux/mod.rs:
+src/aux/ft_sqrt.rs:
+src/aux/ft_powi.rs:
+src/aux/ft_cbrt.rs:
+src/aux/ft_atan.rs:
+src/aux/ft_cos.rs:
+src/aux/ft_abs.rs:
