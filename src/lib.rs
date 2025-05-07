dae5e1241c8ea2b0faae003b4d13bbf3fd6fb680
Luis <luissoto2094@gmail.com>
2025-05-07 12:04:02 +0200

    chore: format

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
-pub mod ft_sqrt;
-pub mod ft_powi;
-pub mod ft_cbrt;
-pub mod ft_atan;
-pub mod ft_cos;
 pub mod ft_abs;
+pub mod ft_atan;
+pub mod ft_cbrt;
+pub mod ft_cos;
+pub mod ft_powi;
+pub mod ft_sqrt;
 
-pub use ft_sqrt::ft_sqrt;
-pub use ft_powi::ft_powi;
-pub use ft_cbrt::ft_cbrt;
-pub use ft_atan::ft_atan;
-pub use ft_cos::ft_cos;
 pub use ft_abs::ft_abs;
+pub use ft_atan::ft_atan;
+pub use ft_cbrt::ft_cbrt;
+pub use ft_cos::ft_cos;
+pub use ft_powi::ft_powi;
+pub use ft_sqrt::ft_sqrt;
